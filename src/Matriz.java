a2ab9792e6ab23b53c887eb5138a2b469db471e4
felipeperes-softwareeng <felipepdoliveira@gmail.com>
2026-05-16 12:56:01 -0300

    Representação matriz codada

---
+++ src/Matriz.java
@@ -0,0 +1,19 @@
+public class Matriz {
+    public static void main(String[] args){
+
+        int[][] numeros = new int[3][3];
+
+        numeros[0][0] = 7;
+        numeros[0][1] = 23;
+        numeros[0][2] = 41;
+
+        numeros[1][0] = 5;
+        numeros[1][1] = 38;
+        numeros[1][2] = 12;
+
+        numeros[2][0] = 47;
+        numeros[2][1] = 19;
+        numeros[2][2] = 34;
+
+    }
+}
